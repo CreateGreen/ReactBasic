62eaedc18edf07a9f836c20af830f28324ce90bf
Jiseok Yong <pqmnvv4@gmail.com>
2022-03-02 16:36:49 +0900

    first

---
+++ Todolist/src/App.js
@@ -0,0 +1,36 @@
+
+import styles from "./App.module.css";
+import {useState, useEffect} from "react";
+
+
+const App=()=>{
+  const [Do,setDo]= useState("");
+  const [Dos,setDos]=useState([]);
+  const onChange=(event)=>{
+    setDo(event.target.value)
+  }
+  const onSubmit =(event)=> {
+    event.preventDefault();
+    if(Do ===""){
+      return;
+    }
+    setDos(currentArray=>[Do,...currentArray]);
+    setDo("");
+    
+  }
+console.log(Dos);
+  return (
+    <div>
+      <h1>My To Do ({Dos.length})</h1>
+      <form onSubmit={onSubmit}>
+      <input onChange={onChange} 
+      value ={Do}
+      type="text" 
+      placeholder="Write your to do ..." />
+      <button>Add to do</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
